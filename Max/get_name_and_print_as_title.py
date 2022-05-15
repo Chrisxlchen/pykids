3d41f4b87781ead037790a6bc964435dbc4b9304
chris <chris.xl.chen@gmail.com>
2022-05-15 12:06:40 -0700

    add assignment get_name_and_print_as_title

---
+++ Max/get_name_and_print_as_title.py
@@ -0,0 +1,18 @@
+"""
+Write a program that read from console and print the read as title
+Run your program, your program will ask user to input their first name and last name. User can input any format.
+Like all capital or all lower case or mixed. Your program will print the name as title.
+Example:
+What's your first name: Chris/CHRIS
+What's your last name: chen/CHEN/cHeN
+Hello Chris Chen!
+"""
+
+
+def get_name_and_print_as_title():
+    # Write you code here
+    pass
+
+
+if __name__ == '__main__':
+    get_name_and_print_as_title()
